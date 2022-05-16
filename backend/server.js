fc9d02ab574ed715201649bfd63f1bac1566a6ce
libbydavis <libstarnz@gmail.com>
2022-05-16 20:25:57 +1200

    changed backend to not use mongoose, loaded db results to page, added sort on two columns

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,27 @@ const cors = require("cors");
 
 const server = express();
 
-connectDB().then((r) => console.log("connected to DB"));
+//const dbConnect = dbo.getDb();
+require("dotenv").config();
+const connectionString = process.env.MONGO_URI;
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(connectionString).then(client => {
+  console.log("Connected to database")
+  const db = client.db("Assign1B");
+
+  server.get("/articles", (req, res) => {
+    db.collection("Articles").find().toArray(function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        res.json(result);
+      }
+    });
+  });
+}).catch(error => console.log(error));
+
 
 server.use(cors());
 server.use(express.json());
@@ -21,5 +41,6 @@ server.post("/login", (req, res) => {
   //   getUsers().then((res) => console.log(res));
   res.send("Logged User In");
 });
+
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- frontend/src/Components/BrowseArticles.js
+++ frontend/src/Components/BrowseArticles.js
@@ -1,7 +1,10 @@
 import Table from 'react-bootstrap/Table'
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function BrowseArticles() {
-    const articles = [
+    const [articles, setArticles] = useState([]);
+    const articles1 = [
         {title: "dogs are cool",
         authors: ["david"],
         yearPublished: "2001",
@@ -22,14 +25,31 @@ function BrowseArticles() {
             researchType: "case study",
             participantType: "dogs"}
     ]
+
+    useEffect(() => {
+        axios.get("http://localhost:8082/articles")
+            .then((res) => {
+                setArticles(res.data);
+            })
+            .catch((er) => {
+            console.log(er);
+        })
+    }, []);
+
+    function sort(sortBy) {
+        let articlesCopy = [...articles];
+        articlesCopy.sort((a, b) => (a[sortBy] > b[sortBy]) ? 1 : -1);
+        setArticles(articlesCopy);
+    }
+
     return (
         <div className="flexCentre">
             <Table className="table-bordered customTableWidth">
                 <thead>
                 <tr>
-                    <th>Title</th>
+                    <th onClick={() => sort("title")}>Title</th>
                     <th>Authors</th>
-                    <th>Year of Publication</th>
+                    <th onClick={() => sort("yearPublished")}>Year of Publication</th>
                     <th>Journal Name</th>
                     <th>SE Practice</th>
                     <th>Claim</th>
