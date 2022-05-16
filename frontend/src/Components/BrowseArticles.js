7afc8f64b46aa3f47b1587d31fe026364cf46d41
libbydavis <libstarnz@gmail.com>
2022-05-16 17:07:36 +1200

    added bootstrap and created table of articles

--- frontend/src/Components/BrowseArticles.js
+++ frontend/src/Components/BrowseArticles.js
@@ -1,5 +1,69 @@
+import Table from 'react-bootstrap/Table'
+
 function BrowseArticles() {
+    const articles = [
+        {title: "dogs are cool",
+        authors: ["david"],
+        yearPublished: "2001",
+        journalName: "cool dogs journal",
+        SEPractice: "Dogs",
+        claim: "dogs are indeed cool",
+        evidenceResult: "agree",
+        researchType: "case study",
+        participantType: "dogs"},
+
+        {title: "cats are cool",
+            authors: ["rachel", "gia"],
+            yearPublished: "2001",
+            journalName: "cool dogs journal",
+            SEPractice: "Dogs",
+            claim: "dogs are indeed cool",
+            evidenceResult: "agree",
+            researchType: "case study",
+            participantType: "dogs"}
+    ]
+    return (
+        <div className="flexCentre">
+            <Table className="table-bordered customTableWidth">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Authors</th>
+                    <th>Year of Publication</th>
+                    <th>Journal Name</th>
+                    <th>SE Practice</th>
+                    <th>Claim</th>
+                    <th>Result of Evidence</th>
+                    <th>Type of Research</th>
+                    <th>Type of Participant</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    articles.map((article, key) => {
+                        return (
+                            <tr key={key}>
+                                <td>{article.title}</td>
+                                <td>{article.authors}</td>
+                                <td>{article.yearPublished}</td>
+                                <td>{article.journalName}</td>
+                                <td>{article.SEPractice}</td>
+                                <td>{article.claim}</td>
+                                <td>{article.evidenceResult}</td>
+                                <td>{article.researchType}</td>
+                                <td>{article.participantType}</td>
+                            </tr>
+                        )
+                    })
+                }
+                </tbody>
+
+            </Table>
+        </div>
+
+    )
 
 }
+
 
 export default BrowseArticles;
--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -2,16 +2,16 @@ import {Link} from "react-router-dom";
 
 function NavBar() {
     return (
-        <div className="navbar">
+        <div className="SPEEDnavbar">
             <div>
                 <div>
                     <Link to="/"><h1 className="title">SPEED Database</h1></Link>
-                    <h3 className="subtitle">find the best articles relevant to your research project</h3>
+                    <h3 className="tagline">find the best articles relevant to your research project</h3>
                 </div>
                 <Link to="/login" className="loginButton buttonLinkStyle">Login</Link>
             </div>
-            <div className="navLinksContainer">
-                <nav className="navlinks">
+            <div className="flexCentre">
+                <nav className="navlinks flexCentre">
                     <Link to="/">Home</Link>
                     <Link to="/browseArticles">Browse Articles</Link>
                     <Link to="/submitArticle">Submit Article</Link>
