53433d1ded7c5cbcb2db101bc84dd3bcd680aeed
Max.venter00 <max.venter00@gmail.com>
2022-05-23 17:30:55 +1200

    Fixed navbar syntax error from merge

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { LoginContext } from "../App";
 import { Link, Navigate } from "react-router-dom";
 import { AppContext } from "../Context";
@@ -7,26 +7,24 @@ function NavBar() {
   const loggedIn = useContext(AppContext);
 
   return (
-    <div className="navbar">
+    <div className="SPEEDnavbar">
       <div>
         <div>
           <Link to="/">
-            <h1 className="title">SPEED Database</h1>
+            <h1 className="navTitle">SPEED Database</h1>
           </Link>
-          <h3 className="subtitle">
+          <h3 className="tagline">
             find the best articles relevant to your research project
           </h3>
         </div>
-        {!loggedIn.loggedIn ? (
-          <>
-            <Link to="/login" className="loginButton buttonLinkStyle">
-              Login
-            </Link>
-          </>
-        ) : null}
+        {loggedIn ? null : (
+          <Link to="/login" className="loginButton buttonLinkStyle">
+            Login
+          </Link>
+        )}
       </div>
-      <div className="navLinksContainer">
-        <nav className="navlinks">
+      <div className="flexCentre">
+        <nav className="navlinks flexCentre">
           <Link to="/">Home</Link>
           <Link to="/browseArticles">Browse Articles</Link>
           <Link to="/submitArticle">Submit Article</Link>
