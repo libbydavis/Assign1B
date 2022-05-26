c654401295f1e1671e7ddf8e511705a75cfc6f2f
libbydavis <libstarnz@gmail.com>
2022-05-26 15:12:06 +1200

    added file upload to populate submit article form. Also added clear form after submission

--- frontend/src/Components/SubmitArticle.js
+++ frontend/src/Components/SubmitArticle.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import axios from "axios";
+import bibtexParse from "bibtex-parse-js";
 
 
 function SubmitArticle() {
@@ -72,6 +73,17 @@ function SubmitArticle() {
                 .then((res) => {
                     if (res.status === 200) {
                         alert("Article Submitted");
+
+                        //clear form
+                        setArticle({
+                            title: "",
+                            authors: "",
+                            yearPublished: "",
+                            journalName: "",
+                            volume: "",
+                            doi: ""
+                        });
+
                     }
                     else if (res.status === 409) {
                         alert("Article Unable To Be Submitted. " + res.data);
@@ -83,18 +95,52 @@ function SubmitArticle() {
         }
 
     }
+
+    function handleFileUpload(e) {
+        let file = e.target.files[0];
+        let fileReader = new FileReader();
+        fileReader.onload = function(e) {
+            console.log(e.target.result);
+
+            let parsedFile = bibtexParse.toJSON(e.target.result);
+
+            populateFields(parsedFile[0].entryTags);
+        }
+        fileReader.readAsText(file);
+
+    }
+
+    function populateFields(data) {
+        const inputTitle = data.title ? data.title : "";
+        const inputAuthors = data.author ? data.author : "";
+        const inputYear = data.year ? data.year : "";
+        const inputJournal = data.journal ? data.journal : "";
+        const inputVolume = data.volume ? data.volume : "";
+        const doi = data.doi ? data.doi : "";
+
+        setArticle({...article,
+            title: inputTitle,
+            authors: inputAuthors,
+            yearPublished: inputYear,
+            journalName: inputJournal,
+            volume: inputVolume,
+            doi: doi
+        });
+    }
+
     return (
         <div className="submitDiv">
             <h2>Submit An Article To SPEED</h2>
+            <input type="file" onChange={handleFileUpload} accept=".bibtex"></input>
             <p style={{display: 'none'}} id="titleError" className="errorP">Please enter a title</p>
-            <input placeholder="title" id="title" onChange={handleChange}></input>
+            <input placeholder="title" id="title" value={article.title} onChange={handleChange}></input>
             <p style={{display: 'none'}} id="authorsError" className="errorP">Please enter at least one author</p>
-            <input placeholder="authors" id="authors" onChange={handleChange}></input>
+            <input placeholder="authors" id="authors" value={article.authors} onChange={handleChange}></input>
             <p style={{display: 'none'}} id="yearError" className="errorP">Please enter four numbers 0-9</p>
             <input placeholder="year published" value={article.yearPublished} id="yearPublished" onChange={handleChange}></input>
-            <input placeholder="journal name" id="journalName" onChange={handleChange}></input>
-            <input placeholder="volume" id="volume" onChange={handleChange}></input>
-            <input placeholder="doi" id="doi" onChange={handleChange}></input>
+            <input placeholder="journal name" id="journalName" value={article.journalName} onChange={handleChange}></input>
+            <input placeholder="volume" id="volume" value={article.volume} onChange={handleChange}></input>
+            <input placeholder="doi" id="doi" value={article.doi} onChange={handleChange}></input>
 
             <button type="button" className="buttonLinkStyle submitButton" onClick={submit}>Submit</button>
         </div>
